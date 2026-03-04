9bf2f2c69f3f477987693b8dcd6607b0be4064c2
poojan-mansuria <poojanmansuria2005@gmail.com>
2026-03-04 15:34:54 +0530

    Factorial series

---
+++ Maths/Math 8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1;// Initliazing first two numbers
+
+    cout << "Fibonacci Series: ";
+
+    for(int i = 1; i <= n; i++) {
+        cout << a << " ";
+
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
