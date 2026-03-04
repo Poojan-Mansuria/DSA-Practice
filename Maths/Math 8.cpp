33562a36ec24bc2bfd40823668c86c0f416698ef
Poojan Mansuria <poojanmansuria2005@gmail.com>
2026-03-04 15:39:18 +0530

    Add files via upload

---
+++ Maths/Math 8.cpp
@@ -0,0 +1,25 @@
+// Fibonnaci Series
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1;// Initliazing first two numbers
+
+    cout << "Fibonacci Series: ";
+
+    for(int i = 1; i <= n; i++) {
+        cout << a << " ";
+
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
