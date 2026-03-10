cef7e6c5e125709bfa3f68b90d2c928d16a3a831
poojan-mansuria <poojanmansuria2005@gmail.com>
2026-03-10 22:10:27 +0530

    Print 1-N Backtracking

---
+++ Recursion/Print-Linearly-From-1_N-Backtracking.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int i,int n){
+    if(i<1) return;
+    else{
+        print(i-1,n);
+        cout<<i;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n,n);
+    return 0;
+}
