be754b0930d360d70d44671f0cd126502d4ed419
poojan-mansuria <poojanmansuria2005@gmail.com>
2026-03-12 17:56:21 +0530

    Fibonnaci of Given No N using multiple calls

---
+++ Recursion/Fibonnaci-Series-Multiple-calls.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fibonnaci(int n){
+    if(n<=1){
+        return n;
+    }
+
+    int last=fibonnaci(n-1);
+    int slast=fibonnaci(n-2);
+    return last+slast;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<fibonnaci(n);
+}
---
+++ Recursion/Sum-Of-Array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arraysum(int arr[], int n, int i){
+
+    if(i==n){
+        return 0;
+    }
+
+    return arr[i] + arraysum(arr,n,i+1);
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int sum = arraysum(arr,n,0);
+
+    cout<<"Sum of array = "<<sum;
+}
