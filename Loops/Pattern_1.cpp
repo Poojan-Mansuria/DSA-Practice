128caf19e0613530ee26fdc8eb03c53cccebcd8d
poojan-mansuria <poojanmansuria2005@gmail.com>
2026-03-17 23:02:17 +0530

    Loop Pattern 1

---
+++ Loops/Pattern_1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* -------- Pattern 1 -------- */
+void pattern1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;   // number of test cases
+
+    for(int k = 0; k < t; k++){   // multiple inputs
+        int n;
+        cin >> n;
+
+        pattern1(n);   // can call any function over here 
+        cout << endl;  // space between patterns
+    }
+
+    return 0;
+}
